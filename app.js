b5698d3bae7752f4829836f7e881d9942622b279
NestorGS <144263733+NestorGS@users.noreply.github.com>
2025-06-03 02:56:23 -0600

    modificando varaibles de entorno

--- app.js
+++ app.js
@@ -23,27 +23,7 @@ console.log("Puerto:", process.env.MYSQLPORT);
 
 app.use(express.static('views')); // Sirve los archivos HTML, CSS, JS
 
-// Conexión a la base de datos con manejo de errores y reconexión
-const db = mysql.createConnection({
-    host: process.env.MYSQLHOST,
-    user: process.env.MYSQLUSER,
-    password: process.env.MYSQLPASSWORD,
-    database: process.env.MYSQLDATABASE,
-    port: process.env.MYSQLPORT
-});
 
-const conectarBD = () => {
-    db.connect((err) => {
-        if (err) {
-            console.error("❌ Error al conectar a la base de datos:", err);
-            setTimeout(conectarBD, 5000); // Reintenta la conexión en 5 segundos
-        } else {
-            console.log("✅ Conexión exitosa a la base de datos.");
-        }
-    });
-};
-
-conectarBD();
 
 // Iniciar servidor
 app.listen(PORT, () => {
