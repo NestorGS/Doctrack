c341837b08400117b50e4715768349e73c5f037b
NestorGS <144263733+NestorGS@users.noreply.github.com>
2025-06-03 02:34:49 -0600

    agregando keep-alive

---
+++ keep-alive.js
@@ -0,0 +1,12 @@
+const keepAlive = () => {
+    setInterval(async () => {
+        try {
+            const response = await fetch("https://host-doctrack-production.up.railway.app/");
+            console.log("Ping enviado, estado:", response.status);
+        } catch (error) {
+            console.error("Error en keep-alive:", error);
+        }
+    }, 300000); // Cada 5 minutos
+};
+
+keepAlive();
