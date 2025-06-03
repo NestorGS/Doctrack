69cec93abfee807c687486789a69ca01bb17cc01
NestorGS <144263733+NestorGS@users.noreply.github.com>
2025-06-03 02:36:54 -0600

    descargando dependecias

--- app.js
+++ app.js
@@ -2,7 +2,6 @@
 require('dotenv').config(); // Cargar variables de entorno
 require("./keep-alive");
 const express = require("express");
-const mysql = require("mysql2");
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const bcrypt = require('bcryptjs');
