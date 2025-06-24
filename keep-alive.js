22ae16085f223223c16e01aa508c8c0733e71ad0
NestorGS <urielgsnin@gmail.com>
2025-06-24 17:42:18 -0600

    agregar exploracion fisica

--- keep-alive.js
+++ keep-alive.js
@@ -1,7 +1,7 @@
 const keepAlive = () => {
     setInterval(async () => {
         try {
-            const response = await fetch("https://host-doctrack-production.up.railway.app/");
+            const response = await fetch("https://doctrack-production.up.railway.app/");
             console.log("Ping enviado, estado:", response.status);
         } catch (error) {
             console.error("Error en keep-alive:", error);
